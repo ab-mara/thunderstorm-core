060494eeb8a1ec5981a50819fc14cd8510098e87
rhinonix <1107968+rhinonix@users.noreply.github.com>
2023-12-27 07:50:20 +0100

    Updated

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,8 +1,9 @@
 export default async (event) => {
   try {
     const test =
-      event.queryStringParameters?.test ??
-      "This is a test. The query string parameter 'test' was not provided.";
+      event.queryStringParameters && event.queryStringParameters.test
+        ? event.queryStringParameters.test
+        : "This is a test. The query string parameter 'test' was not provided.";
     return new Response(test);
   } catch (error) {
     console.error("Error processing request:", error);
