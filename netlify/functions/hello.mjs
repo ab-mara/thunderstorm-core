170cb0d8209a365c741f946dd564b61fb1485fd0
rhinonix <1107968+rhinonix@users.noreply.github.com>
2024-01-08 11:15:31 +0100

    Updated

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -2,7 +2,7 @@ export default async (event) => {
   try {
     const test = event.queryStringParameters?.test;
     console.log(event.queryStringParameters);
-    console.log("test: ");
+    console.log("test");
     let responseText = test
       ? `Received test parameter: ${test}`
       : "This is a test. The query string parameter 'test' was not provided.";
