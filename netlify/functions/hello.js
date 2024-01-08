b318732760a947f309fc56a6e2c62df043371830
rhinonix <1107968+rhinonix@users.noreply.github.com>
2024-01-08 13:59:34 +0100

    Updated

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,10 @@
 exports.handler = async (event) => {
+  const SUPABASE_API_KEY = Netlify.env.get("SUPABASE_API_KEY");
+  const SUPABASE_URL = Netlify.env.get("SUPABASE_URL");
   const querystring = event.queryStringParameters;
   const test = querystring.test;
   let responseText = test
-    ? `Received test parameter: ${test}`
+    ? `Received test parameter: ${test} from Netlify environment variables: SUPABASE_API_KEY: ${SUPABASE_API_KEY} and SUPABASE_URL: ${SUPABASE_URL}`
     : "This is a test. The query string parameter 'test' was not provided.";
 
   return {
