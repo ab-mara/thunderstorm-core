4627bd8d1d25588bfc4d21d00142716a3667ceb6
rhinonix <1107968+rhinonix@users.noreply.github.com>
2024-01-08 11:41:58 +0100

    Updated

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,13 +1,12 @@
 exports.handler = async (event) => {
-  try {
-    const test = event.queryStringParameters?.test;
-    let responseText = test
-      ? `Received test parameter: ${test}`
-      : "This is a test. The query string parameter 'test' was not provided.";
-    //responseText = "foo";
-    return new Response(responseText);
-  } catch (error) {
-    console.error("Error processing request:", error);
-    return new Response("Internal Server Error", { status: 500 });
-  }
+  const querystring = event.queryStringParameters;
+  const test = querystring.test;
+  let responseText = test
+    ? `Received test parameter: ${test}`
+    : "This is a test. The query string parameter 'test' was not provided.";
+
+  return {
+    statusCode: 200,
+    body: `${responseText}`,
+  };
 };
